00332f798df60dc06f6c997b55ee8642a982f536
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-06 15:57:12 +0700

    update

--- src/product-service/routes/product.routes.js
+++ src/product-service/routes/product.routes.js
@@ -4,9 +4,9 @@ const auth = require("../../middleware/auth.middleware");
 const roleAdmin = require("../../middleware/admin.middleware");
 
 //private
-router.get("/", auth, ProductController.getAll);
+router.get("/", ProductController.getAll);
 router.post("/", auth, roleAdmin, ProductController.create);
-router.get("/:id", auth, ProductController.getById);
+router.get("/:id", ProductController.getById);
 router.put("/:id", auth, roleAdmin, ProductController.update);
 router.delete("/:id", auth, roleAdmin, ProductController.delete);
 router.get(
